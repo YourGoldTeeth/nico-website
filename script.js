5f5c4c7fa6875d4655474ba21bc6689ae1e766cc
James West <jameseswest@gmail.com>
2024-09-03 21:12:44 -0500

    added carousel

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// This script can be used to add dynamic features in the future.
-console.log("Welcome to Nico West's Fun Page!");
+let currentSlide = 0; // Initial slide index
+
+function changeSlide(n) {
+  const slides = document.getElementsByClassName("carousel-item");
+  currentSlide += n; // Increment or decrement slide index
+
+  // Wrap around the slides if the end is reached
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  // Update the transform property to show the current slide
+  const carouselImages = document.querySelector(".carousel-images");
+  carouselImages.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
